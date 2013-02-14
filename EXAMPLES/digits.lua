d1e9a8ca562ee9796fe6b3632007caf5eef5eaca
Paco Zamora Martinez <pakozm@gmail.com>
2013-02-14 16:01:13 +0100

    Modified digits example

--- EXAMPLES/digits.lua
+++ EXAMPLES/digits.lua
@@ -1,38 +1,17 @@
--- un generador de valores aleatorios... y otros parametros
-bunch_size    = tonumber(arg[1]) or 64
-semilla       = 1234
-aleat         = random(semilla)
-description   = "256 inputs 256 tanh 128 tanh 10 softmax"
-inf           = -1
-sup           =  1
-otrorand      = random(5678)
-learning_rate = 0.01
-momentum      = 0.01
-weight_decay  = 1e-05
-max_epochs    = 10
+digits_image = ImageIO.read(string.get_path(arg[0]).."digits.png")
+m1           = digits_image:to_grayscale():invert_colors():matrix()
 
---------------------------------------------------------------
-m1 = ImageIO.read("digits.png"):to_grayscale():invert_colors():matrix()
-train_input = dataset.matrix(m1,
-			     {
-			       patternSize = {16,16},
-			       offset      = {0,0},
-			       numSteps    = {80,10},
-			       stepSize    = {16,16},
-			       orderStep   = {1,0}
-			     })
-
-val_input  = dataset.matrix(m1,
-			    {
-			      patternSize = {16,16},
-			      offset      = {1280,0},
-			      numSteps    = {20,10},
-			      stepSize    = {16,16},
-			      orderStep   = {1,0}
-			    })
+-- TRAINING --
 -- a simple matrix for the desired output
+train_input  = dataset.matrix(m1,
+			      {
+				patternSize = {16,16},
+				offset      = {0,0},
+				numSteps    = {80,10},
+				stepSize    = {16,16},
+				orderStep   = {1,0}
+			      })
 m2 = matrix(10,{1,0,0,0,0,0,0,0,0,0})
-
 -- a circular dataset which advances with step -1
 train_output = dataset.matrix(m2,
 			      {
@@ -42,7 +21,15 @@ train_output = dataset.matrix(m2,
 				stepSize    = {-1},
 				circular    = {true}
 			      })
-
+-- VALIDATION --
+val_input = dataset.matrix(m1,
+			   {
+			     patternSize = {16,16},
+			     offset      = {1280,0},
+			     numSteps    = {20,10},
+			     stepSize    = {16,16},
+			     orderStep   = {1,0}
+			   })
 val_output   = dataset.matrix(m2,
 			      {
 				patternSize = {10},
@@ -51,7 +38,6 @@ val_output   = dataset.matrix(m2,
 				stepSize    = {-1},
 				circular    = {true}
 			      })
-
 
 thenet = ann.mlp.all_all.generate{
   bunch_size  = 32,
@@ -98,7 +84,8 @@ result = ann.train_crossvalidation{ ann = thenet,
 --     return thenet:validate_dataset(t)
 --   end
 clock:stop()
-cpu,wall = clock:read()
-printf("# Wall total time: %.3f    per epoch: %.3f\n", wall, wall/max_epochs)
-printf("# CPU  total time: %.3f    per epoch: %.3f\n", cpu, cpu/max_epochs)
-printf("# Validation error: %f", result.best_val_error)
+cpu,wall   = clock:read()
+num_epochs = result.last_epoch
+printf("# Wall total time: %.3f    per epoch: %.3f\n", wall, wall/num_epochs)
+printf("# CPU  total time: %.3f    per epoch: %.3f\n", cpu, cpu/num_epochs)
+printf("# Validation error: %f\n", result.best_val_error)
