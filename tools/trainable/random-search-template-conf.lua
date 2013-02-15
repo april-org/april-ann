39ff186cffca3c72593b975646e6e25da4566c25
Paco Zamora Martinez <pakozm@gmail.com>
2013-02-15 14:45:09 +0100

    Modified random search template

--- tools/trainable/random-search-template-conf.lua
+++ tools/trainable/random-search-template-conf.lua
@@ -1,7 +1,7 @@
 return {
   fixed_params = {
     { option="-x", value=1, tag="x"  },
-    { option="-y", value=2, tag="y"  },
+    { option="-y", value=2, tag="y", hidden=true },
   },
   random_params = {
     { option="-a", tag="a", sampling = "uniform", type="integer",
@@ -11,7 +11,7 @@ return {
     { option="-c", tag="c", sampling = "uniform", values= { "n", "m", "l", "o" } },
     { option="-d", tag="d", sampling = "gaussian", prec=3, values= { mean=5, variance=0.1 } },
     { option="-e", tag="e", sampling = "random" },
-    { option=nil, tag="metaparameter", sampling=ANY ...  },
+    { option=nil,  tag="metaparameter", sampling=ANY ...  },
     { option=ANY, check=function(params) return true end, tag=ANY,
       sampling=ANY ...  },
   },
