b00489504e01e17daeb520a70b09a213cabf263e
Joan Pastor <joapaspe@gmail.com>
2013-04-25 18:13:28 +0200

    Now the point classifier can work with 1 white data

--- packages/imaging/interest_points/test/test_interest_points2.lua
+++ packages/imaging/interest_points/test/test_interest_points2.lua
@@ -4,6 +4,7 @@ bunchsize   = arg[3] or 64
 
 img = ImageIO.read(imgFile):to_grayscale()
 -- Load the image
+print (img_inv, img)
 w, h = img:geometry()
 print(mlpFile)
 mlp = ann.mlp.all_all.load(mlpFile, bunchsize)
@@ -17,25 +18,40 @@ print(#lowers)
 outFile = string.remove_extension(imgFile)..".svg" 
 point = uppers[1]
 
-pc = interest_points.pointClassifier(500,250,50, 30)
+
+pc = interest_points.pointClassifier(500,250,50, 30, false)
 
 
 res = pc:compute_point(img, point, mlp)
+cl = pc:classify_point(img, point, mlp)
 
-print(res)
-for i, v in ipairs(res) do
-    print(v)
+
+-- Classify uppers
+local uppers_classified = pc:classify_points(img, uppers, mlp)
+--local uppers_res  = pc:compute_points(img_inv, uppers, mlp)
+
+--- Transform in tables
+local tables = interest_points.sort_by_class(uppers_classified, 5)
+mySVG:addPointsFromTables(tables)
+
+print("Tables: ", #tables)
+for i=1, 5 do
+  print(i, #tables[i])
 end
---[[for _, p in ipairs(uppers) do
-mySVG:addSquare(p, {})
 
+-- Classify lowers
+local lowers_classified = pc:classify_points(img, lowers, mlp)
+local lower_res  = pc:compute_points(img, uppers, mlp)
+
+--- Transform in tables
+tables = interest_points.sort_by_class(lowers_classified, 5)
+mySVG:addPointsFromTables(tables)
+
+print("Tables: ", #tables)
+for i=1, 5 do
+  print(i, #tables[i])
 end
-for _, p in ipairs(lowers) do
-mySVG:addSquare(p, {color = "red"})
-
-
-end
+--]]
 mySVG:write(outFile)
 print(#uppers)
 print(#lowers)
---]]
