c59a5603af3c4523bd405a41cac3f4282be9e95f
Paco Zamora Martinez <pakozm@gmail.com>
2013-05-07 12:03:44 +0200

    Examples following new achitecture

--- EXAMPLES/xor.lua
+++ EXAMPLES/xor.lua
@@ -1,12 +1,13 @@
-thenet = ann.mlp.all_all.generate{
-  topology   =  "2 inputs 2 logistic 1 logistic",
+bunch_size=4
+thenet  = ann.mlp.all_all.generate("2 inputs 2 logistic 1 logistic")
+trainer = trainable.supervised_trainer(thenet, ann.loss.mse(1), bunch_size)
+trainer:build()
+trainer:randomize_weights{
   random     =  random(1234),
-  use_fanin  =  false,
   inf        = -0.1,
-  sup        =  0.1,
-  bunch_size =  4 }
+  sup        =  0.1 }
 
-thenet:set_option("learning_rate", 1.0)
+thenet:set_option("learning_rate", 8.0)
 thenet:set_option("momentum",      0.5)
 thenet:set_option("weight_decay",  1e-05)
 
@@ -24,8 +25,8 @@ ds_output = dataset.matrix(m_xor, {offset={0,2}, patternSize={1,1}})
 
 rnd=random(523)
 for i=1,10000 do
-  local error = thenet:train_dataset{ input_dataset  = ds_input,
-				      output_dataset = ds_output,
-				      shuffle        = rnd }
+  local error = trainer:train_dataset{ input_dataset  = ds_input,
+				       output_dataset = ds_output,
+				       shuffle        = rnd }
   print(i, error)
 end
