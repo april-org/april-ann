8d353883282ef21f121fb3322e29af8a1e4349c9
Paco Zamora Martinez <pakozm@gmail.com>
2013-06-02 20:57:57 +0200

    First steps to improve matrix. Private properties in order to generalize element access for sub-matrix abstraction

---
+++ packages/basics/util/c_src/error_print.cc
@@ -0,0 +1,15 @@
+#include "error_print.h"
+extern "C" {
+#include "lua.h"
+}
+
+extern lua_State *globalL;
+
+void print_lua_tracebak() {
+  lua_getfield(globalL, LUA_GLOBALSINDEX, "debug");
+  lua_getfield(globalL, -1, "traceback");
+  lua_pushvalue(globalL, 1);
+  lua_pushinteger(globalL, 2);
+  lua_call(globalL, 2, 1);
+  fprintf(stderr, "%s\n", lua_tostring(globalL, -1));
+}
