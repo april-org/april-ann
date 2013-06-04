961aa3872daa03de6da99b605df7f1851f02681c
Paco Zamora Martinez <pakozm@gmail.com>
2013-06-04 19:04:04 +0200

    Modified header of source files

--- packages/imaging/image_cleaning/c_src/image_cleaning.h
+++ packages/imaging/image_cleaning/c_src/image_cleaning.h
@@ -1,6 +1,6 @@
 /*
- * This file is part of the Neural Network modules of the APRIL toolkit (A
- * Pattern Recognizer In Lua).
+ * This file is part of APRIL-ANN toolkit (A
+ * Pattern Recognizer In Lua with Artificial Neural Networks).
  *
  * Copyright 2013, Joan Pastor- Pellicer, Salvador España-Boquera, Francisco
  * Zamora-Martinez
