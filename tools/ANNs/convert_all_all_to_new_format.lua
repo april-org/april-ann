1ea8fd0abc37ddf74f4c62bdb8beb02ad9567c0c
pakozm <pakozm@gmail.com>
2013-06-11 12:55:34 +0200

    Changing learning rate protocol

--- tools/ANNs/convert_all_all_to_new_format.lua
+++ tools/ANNs/convert_all_all_to_new_format.lua
@@ -3,4 +3,5 @@ output_filename = arg[2]
 bunch_size      = tonumber(arg[3] or 32)
 thenet  = ann.mlp.all_all.load(input_filename)
 trainer = trainable.supervised_trainer(thenet, nil, bunch_size)
+trainer:build()
 trainer:save(output_filename)
