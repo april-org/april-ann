00b3350baf6144f73308af8f6432293ddba343bc
Paco Zamora Martinez <pakozm@gmail.com>
2013-06-16 16:59:17 +0200

    Improved test script for sampling

--- packages/ann/sdae/test/test-sampling.lua
+++ packages/ann/sdae/test/test-sampling.lua
@@ -1,7 +1,11 @@
-n = tonumber(arg[1] or 1)     -- validation pattern (up to 200)
-p = tonumber(arg[2] or 0.5)   -- noise percentage
-a = tonumber(arg[3] or 0.004) -- SGD alpha parameter
-s = tonumber(arg[4] or 12345) -- random seed
+pnoise        = tonumber(arg[1] or 0.5)   -- noise percentage
+loss_function = arg[2] or "cross_entropy"
+alpha         = tonumber(arg[3] or 0.01)  -- SGD alpha parameter
+seed          = tonumber(arg[4] or 12345) -- random seed
+
+num_repetitions = 10
+max_iterations  = 10
+stop_criterion  = 1e-08
 
 m1 = ImageIO.read("digits.png"):to_grayscale():invert_colors():matrix()
 
@@ -13,52 +17,108 @@ val_input  = dataset.matrix(m1,
 			      stepSize    = {16,16},
 			      orderStep   = {1,0}
 			    })
-
+rnd       = random(seed)
 trainer   = trainable.supervised_trainer.load("full-sdae.net")
 full_sdae = trainer:get_component()
+if loss_function == "local_fmeasure" then
+  loss = ann.loss.local_fmeasure{ size=1, beta=2 }
+  log  = false
+  full_sdae:pop()
+  full_sdae:push(ann.components.actf.logistic{ name = "actf-output" })
+  trainer:build()
+elseif loss_function ~= "mse" then
+  loss = ann.loss[loss_function](full_sdae:get_output_size())
+  log  = true
+else
+  loss = ann.loss.mse(full_sdae:get_output_size())
+  log  = false
+  full_sdae:pop()
+  full_sdae:push(ann.components.actf.logistic{ name = "actf-output" })
+  trainer:build()
+end
 
-full_sdae:pop()
-full_sdae:push(ann.components.actf.logistic{ name = "actf-output" })
-trainer:build()
+local ite_loss_stat = stats.mean_var()
+local sgd_loss_stat = stats.mean_var()
 
-rnd       = random(s)
-input     = val_input:getPattern(n)
-loss      = ann.loss.local_fmeasure{ size=1, beta=2 }
-mask      = {}
-for i=1,#input do
-  v = rnd:rand(1.0)
-  if v < p then input[i] = rnd:rand(1.0)
-  else table.insert(mask, i)
+local ite_quartiles = {}
+local sgd_quartiles = {}
+
+for ipat,input in val_input:patterns() do
+  fprintf(io.stderr,"\r%3.0f%%", ipat/val_input:numPatterns()*100)
+  io.stderr:flush()
+  local target_class     = (ipat-1) % 10
+  local current_ite_loss = 0
+  local current_sgd_loss = 0
+  for rep=1,num_repetitions do
+    mask = {}
+    for i=1,#input do
+      v = rnd:rand(1.0)
+      if v < pnoise then input[i] = rnd:rand(1.0)
+      else table.insert(mask, i)
+      end
+    end
+    output,L = ann.autoencoders.iterative_sampling{
+      model   = full_sdae,
+      input   = input,
+      max     = max_iterations,
+      mask    = mask,
+      stop    = stop_criterion,
+      verbose = false,
+      log     = log,
+      loss    = loss,
+    }
+    -- matrix.saveImage(matrix(16,16,output), "wop1.pnm")
+    -- output = table.imap(output, math.log)
+    -- print(loss:loss(tokens.memblock(output),
+    -- tokens.memblock(val_input:getPattern(n))), L)
+    loss:reset()
+    ite_L = loss:loss(tokens.memblock(output), tokens.memblock(input))
+    ite_loss_stat:add(ite_L)
+    table.insert(ite_quartiles, ite_L)
+    
+    output,L = ann.autoencoders.sgd_sampling{
+      model   = full_sdae,
+      input   = input,
+      max     = max_iterations,
+      mask    = mask,
+      stop    = stop_criterion,
+      verbose = false,
+      alpha   = alpha,
+      log     = log,
+      clamp   = function(v) return math.max(0, math.min(1,v)) end,
+	loss    = loss,
+    }
+    -- matrix.saveImage(matrix(16,16,output), "wop2.pnm")
+    -- output = table.imap(output, math.log)
+    -- print(loss:loss(tokens.memblock(output),
+    -- tokens.memblock(val_input:getPattern(n))), L)
+    loss:reset()
+    sgd_L = loss:loss(tokens.memblock(output), tokens.memblock(input))
+    sgd_loss_stat:add(sgd_L)
+    table.insert(sgd_quartiles, sgd_L)
   end
 end
-output,L = ann.autoencoders.iterative_sampling{
-  model   = full_sdae,
-  input   = input,
-  max     = 1000,
-  mask    = mask,
-  stop    = 1e-08,
-  verbose = false,
-  log     = false,
-  loss    = loss,
-}
-matrix.saveImage(matrix(16,16,output), "wop1.pnm")
--- output = table.imap(output, math.log)
-print(loss:loss(tokens.memblock(output),
-		tokens.memblock(val_input:getPattern(n))), L)
-
-output,L = ann.autoencoders.sgd_sampling{
-  model   = full_sdae,
-  input   = input,
-  max     = 1000,
-  mask    = mask,
-  stop    = 1e-08,
-  verbose = false,
-  alpha   = a,
-  log     = false,
-  clamp   = function(v) return math.max(0, math.min(1,v)) end,
-  loss    = loss,
-}
-matrix.saveImage(matrix(16,16,output), "wop2.pnm")
--- output = table.imap(output, math.log)
-print(loss:loss(tokens.memblock(output),
-		tokens.memblock(val_input:getPattern(n))), L)
+fprintf(io.stderr,"\n")
+io.stderr:flush()
+table.sort(ite_quartiles)
+table.sort(sgd_quartiles)
+local q1 = math.floor(#ite_quartiles*0.25)
+local q2 = math.floor(#ite_quartiles*0.5)
+local q3 = math.floor(#ite_quartiles*0.75)
+local ite_loss_mean,ite_loss_var = ite_loss_stat:compute()
+local sgd_loss_mean,sgd_loss_var = sgd_loss_stat:compute()
+printf("%.1f  ITE: m= %.4f s2= %.4f Qs= %.4f %.4f %.4f %.4f %.4f  "..
+	 "SGD: m= %.4f s2= %.4f Qs= %.4f %.4f %.4f %.4f %.4f\n",
+       pnoise,
+       ite_loss_mean, ite_loss_var,
+       ite_quartiles[1],
+       ite_quartiles[q1],
+       ite_quartiles[q2],
+       ite_quartiles[q3],
+       ite_quartiles[#ite_quartiles],
+       sgd_loss_mean, sgd_loss_var,
+       sgd_quartiles[1],
+       sgd_quartiles[q1],
+       sgd_quartiles[q2],
+       sgd_quartiles[q3],
+       sgd_quartiles[#sgd_quartiles])
