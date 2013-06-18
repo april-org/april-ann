b080a679cb0456afc97249253c70d8916a1f7e73
Paco Zamora Martinez <pakozm@gmail.com>
2013-06-18 12:01:12 +0200

    Added salt and pepper component. Improved SDAE sampling

---
+++ packages/ann/ann_base/c_src/salt_and_pepper_component.h
@@ -0,0 +1,69 @@
+/*
+ * This file is part of APRIL-ANN toolkit (A
+ * Pattern Recognizer In Lua with Artificial Neural Networks).
+ *
+ * Copyright 2013, Francisco Zamora-Martinez
+ *
+ * The APRIL-ANN toolkit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+#ifndef SALTANDPEPPERCOMPONENT_H
+#define SALTANDPEPPERCOMPONENT_H
+
+#include "vector.h"
+#include "ann_component.h"
+#include "token_vector.h"
+#include "token_memory_block.h"
+#include "MersenneTwister.h"
+
+using april_utils::vector;
+
+namespace ANN {
+  
+  class SaltAndPepperANNComponent : public ANNComponent {
+    MTRand           *random;
+    TokenMemoryBlock *input, *output;
+    Token            *error_input, *error_output;
+    float            zero, one;
+    float            prob;
+    
+  public:
+    SaltAndPepperANNComponent(MTRand *random, float zero, float one, float prob,
+			      const char *name=0,
+			      unsigned int size=0);
+    virtual ~SaltAndPepperANNComponent();
+    
+    virtual Token *getInput() { return input; }
+    virtual Token *getOutput() { return output; }
+    virtual Token *getErrorInput() { return error_input; }
+    virtual Token *getErrorOutput() { return error_output; }
+    
+    virtual Token *doForward(Token* input, bool during_training);
+    
+    virtual Token *doBackprop(Token *input_error);
+    
+    virtual void reset();
+    
+    virtual ANNComponent *clone();
+
+    virtual void build(unsigned int _input_size,
+		       unsigned int _output_size,
+		       hash<string,Connections*> &weights_dict,
+		       hash<string,ANNComponent*> &components_dict);
+
+    virtual char *toLuaString();
+  };
+}
+
+#endif // SALTANDPEPPERCOMPONENT_H
