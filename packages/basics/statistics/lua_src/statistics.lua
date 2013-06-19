7a6eebef07590b98ab32c26494316246714b26a0
Joan Pastor <joapaspe@gmail.com>
2013-06-19 17:29:04 +0200

    New class confusion matrix, for general classification stats

--- packages/basics/statistics/lua_src/statistics.lua
+++ packages/basics/statistics/lua_src/statistics.lua
@@ -90,3 +90,185 @@ function stats.mean_var:compute()
   var  = (self.accum_sum2 - self.N * aux_mean * aux_mean) / (self.N - 1)
   return mean,var
 end
+
+--------------------
+-- Confusion Matrix
+-- -----------------
+class("stats.confusion_matrix")
+
+
+april_set_doc("stats.confunsion_matrix", {
+    class       = "class",
+    summary     = "class for computing confusion matrix and classification metrics",
+    description ={
+        "This class is designed to store a confusion matrix and compute main metrics for classification stats",
+    }, })
+
+    ----------------------------------------
+    function stats.confusion_matrix:__call(num_classes)
+
+        local confusion = {}
+        for i = 1, num_classes do
+            local t = {}
+            for j = 1, num_classes do
+                table.insert(t, 0)
+            end
+            table.insert(confusion, t)
+        end
+        local obj = {
+            num_classes = num_classes,
+            confusion = confusion,
+            hits = 0,
+            misses = 0,
+            samples = 0
+        }
+        class_instance(obj, self, true)
+        return obj
+    end
+
+    function stats.confusion_matrix:reset()
+
+        for i = 1, self.num_classes do
+            local t = {}
+            for j = 1, self.num_classes do
+                self.confusion[i][j] = 0
+            end
+        end
+        self.hits = 0
+        self.misses = 0
+        self.samples = 0
+    end
+
+    function stats.confusion_matrix:checkType(clase)
+        return type(clase) == "number" and clase >= 1 and clase <= self.num_classes or false
+    end
+    ---------------------------------------------
+    function stats.confusion_matrix:addSample(pred, gt)
+        if not self:checkType(pred) or not self:checkType(gt) then
+            error("The class is not correct")
+        end
+
+        if gt == pred then
+            self.hits = self.hits + 1
+        else
+            self.misses = self.misses + 1
+        end
+        self.samples = self.samples + 1
+
+        self.confusion[gt][pred] = self.confusion[gt][pred] + 1
+    end
+
+    ------------------------------------------------
+
+    function stats.confusion_matrix:printConfusion(numClasses)
+        for i,v in ipairs(self.confusion) do
+            print(table.concat(v, "\t"))
+        end
+    end
+
+    --------------------------------------------
+    function stats.confusion_matrix.twoTablesIterator(table_pred, table_gt)
+        local i = 0
+        local n = #table_pred
+        return function()
+            i = i+1
+            if i <= n then return table_pred[i],table_gt[i] end
+        end
+
+    end
+    ----------------------------------------------------------------
+    function stats.confusion_matrix.oneTableIterator(typeTable)
+
+        local i = 0
+        local n = #typeTable
+        return function()
+            i = i+1
+            if i <= n then return typeTable[i][1], typeTable[i][2] end
+        end
+
+    end
+    -------------------------------------------------------------
+    function stats.confusion_matrix:addData(param1, param2)
+
+        local iterator
+        if( type(param1) == 'function') then
+            iterator = param1
+            assert(param2 == nil)
+        else
+            iterator = stats.confusion_matrix.twoTablesIterator(param1, param2)
+            assert(type(param1) == "table" and type(param2) == "table", "The type of the params is not correct")
+            assert(#param1, #param2, "The tables does not have the same dimension")
+        end
+
+
+        for pred, gt in iterator do
+            print (pred, gt)
+            self:addSample(pred, gt)
+        end
+    end
+
+    ---------------------------------------------------------------
+    function stats.confusion_matrix:getError()
+      return self.misses/self.samples
+    end
+
+    function stats.confusion_matrix:getAccuracy()
+      return self.hits/self.samples
+    end
+
+    --------------------------------------------------------------
+    function stats.confusion_matrix:getConfusionTables()
+      return self.confusion
+    end
+    ------------------------------------------------------------
+    --
+    function stats.confusion_matrix:getPrecision(tipo)
+
+        local tp = 0
+        local den = 0
+
+        -- Moving by columns
+        for i=1, self.num_classes do
+            v = self.confusion[i][tipo]
+            if i == tipo then
+                tp = v
+
+            end
+                den = den + v
+        end     
+        if den == 0 then
+           return 0
+        end
+        return tp/den
+    end
+
+    function stats.confusion_matrix:getRecall(tipo)
+
+        local tp = 0
+        local den = 0
+
+        -- Moving by columns
+        for j=1, self.num_classes do
+            v = self.confusion[tipo][j]
+            if j == tipo then
+                tp = v
+
+            end
+            den = den + v
+        end 
+
+        if den == 0 then
+            return 0
+        end
+        return tp/den
+    end
+
+    function stats.confusion_matrix:getFMeasure(tipo, beta)
+        local nBeta = beta or 1
+        nBeta = nBeta*nBeta
+        local PR = self:getRecall(tipo)
+        local RC = self:getPrecision(tipo)
+
+        return (1+nBeta)*(PR*RC)/(nBeta*PR+RC)
+    end
+
---
+++ packages/basics/statistics/test/confusionMatrix.lua
@@ -0,0 +1,34 @@
+-- Check 4 clases with table
+
+local N = 4
+local myConf = stats.confusion_matrix(N)
+print (myConf)
+local myPred = { 1, 2, 3, 4, 1, 2 , 3 , 4}
+local myGT   = { 2, 1, 2, 1, 1, 2,  3,  1}
+
+myConf:addData(myPred, myGT)
+print("Raw Confusion Matrix")
+myConf:printConfusion()
+
+
+myConf:reset()
+print("Empty Matrix")
+myConf:printConfusion()
+
+local t = {}
+for i, v in ipairs(myPred) do
+  table.insert(t, {v,myGT[i]})
+end
+myConf:addData(stats.confusion_matrix.oneTableIterator(t))
+
+print("Raw Confusion Matrix")
+myConf:printConfusion()
+
+print("Results")
+
+print("Error ", myConf:getError())
+print("Accuracy ", myConf:getAccuracy())
+
+for i = 1, N do
+  print(i, "PR:", myConf:getPrecision(i), "RC:", myConf:getRecall(i),"FM:", myConf:getFMeasure(i))
+end
