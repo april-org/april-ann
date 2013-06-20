3255c196a1dc95f56ac7bc70d6dc1ce91e94d3a0
Joan Pastor <joapaspe@gmail.com>
2013-06-20 11:56:13 +0200

    Documentation of confusion matrix

--- packages/basics/statistics/lua_src/statistics.lua
+++ packages/basics/statistics/lua_src/statistics.lua
@@ -97,257 +97,323 @@ end
 class("stats.confusion_matrix")
 
 
-april_set_doc("stats.confunsion_matrix", {
+april_set_doc("stats.confusion_matrix", {
     class       = "class",
     summary     = "class for computing confusion matrix and classification metrics",
     description ={
         "This class is designed to store a confusion matrix and compute main metrics for classification stats",
-    }, })
+    },
+})
 
-    ----------------------------------------
-    function stats.confusion_matrix:__call(num_classes, map_table)
+april_set_doc("stats.confusion_matrix.__call", {
+    class ="method",
+    summary     = "Constructor of confusion matrix.",
+    description ={
+        "This class is designed to store a confusion matrix and compute main metrics for classification stats",
+    },
+    params = {
+        "A number of classes [mandatory].",
+        "A table of size num_classes, with the elements on the set.",
+        outputs = {"A confusion_matrix object"}
 
-        local confusion = {}
-        for i = 1, num_classes do
-            local t = {}
-            for j = 1, num_classes do
-                table.insert(t, 0)
-            end
-            table.insert(confusion, t)
+    }
+})
+function stats.confusion_matrix:__call(num_classes, map_table)
+
+    local confusion = {}
+    for i = 1, num_classes do
+        local t = {}
+        for j = 1, num_classes do
+            table.insert(t, 0)
         end
-        
-
-        if (map_table) then
-          assert(#map_table == num_classes, "The map table doesn't have the exact size")
-          map_dict = {}
-
-          for i, v in ipairs(map_table) do
-            map_dict[v] = i
-          end
-          
-        end
-
-        local obj = {
-            num_classes = num_classes,
-            confusion = confusion,
-            hits = 0,
-            misses = 0,
-            samples = 0,
-            -- FIXME: IS NOT POSSIBLE USE MAP DICT AS NIL
-            map_dict = map_dict or -1
-
-        }
-        class_instance(obj, self, true)
-        return obj
+        table.insert(confusion, t)
     end
 
-    function stats.confusion_matrix:reset()
 
-        for i = 1, self.num_classes do
-            local t = {}
-            for j = 1, self.num_classes do
-                self.confusion[i][j] = 0
-            end
-        end
-        self.hits = 0
-        self.misses = 0
-        self.samples = 0
-    end
+    if (map_table) then
+        assert(#map_table == num_classes, "The map table doesn't have the exact size")
+        map_dict = table.invert(map_table)
 
-    function stats.confusion_matrix:checkType(clase)
-        return type(clase) == "number" and clase >= 1 and clase <= self.num_classes or false
-    end
-    ---------------------------------------------
-    function stats.confusion_matrix:addSample(pred, gt)
-
-        if self.map_dict ~= -1 then
-          pred = map_dict[pred]
-          gt   = map_dict[gt]
-        end
-        if not self:checkType(pred) or not self:checkType(gt) then
-            error("The class is not correct")
-        end
-
-        if gt == pred then
-            self.hits = self.hits + 1
-        else
-            self.misses = self.misses + 1
-        end
-        self.samples = self.samples + 1
-
-        self.confusion[gt][pred] = self.confusion[gt][pred] + 1
-    end
-
-    ------------------------------------------------
-
-    function stats.confusion_matrix:printConfusionRaw()
-        
-        for i,v in ipairs(self.confusion) do
-            print(table.concat(v, "\t"))
-        end
-    end
-
-    function stats.confusion_matrix:printConfusion(tags)
-
-        printf("\t|\t Predicted ")
-        for i = 1, self.num_classes do
-            printf("\t\t")
-        end
-
-        printf("|\n")
-        printf("______\t|")
-        for i = 1, self.num_classes do
-            printf("\t___\t")
-        end
-
-        printf("\t___\t|\n")
-        for i,v in ipairs(self.confusion) do
-             
-            local tag = i
-            if tags then
-                tag = tags[i]
-            end
-            printf("%s\t|\t", tag)
-
-            printf("%s\t|\t %0.4f\t|\n", table.concat(v, "\t|\t"), self:getRecall(i))
-        end
-        printf("______\t|")
-        for i = 1, self.num_classes do
-            printf("\t___\t")
-        end
-
-        printf("\t___\t|\n")
-        printf("\t|")
-        for i = 1, self.num_classes do
-            printf("\t%0.4f\t|", self:getPrecision(i))
-        end
-        printf("\t%0.4f\t|\n", self:getError())
-    end
-    --------------------------------------------
-    function stats.confusion_matrix.twoTablesIterator(table_pred, table_gt)
-        local i = 0
-        local n = #table_pred
-        return function()
-            i = i+1
-            if i <= n then return table_pred[i],table_gt[i] end
-        end
+        --for i, v in ipairs(map_table) do
+        --  map_dict[v] = i
+        --end
 
     end
-    ----------------------------------------------------------------
-    function stats.confusion_matrix.oneTableIterator(typeTable)
 
-        local i = 0
-        local n = #typeTable
-        return function()
-            i = i+1
-            if i <= n then return typeTable[i][1], typeTable[i][2] end
-        end
+    local obj = {
+        num_classes = num_classes,
+        confusion = confusion,
+        hits = 0,
+        misses = 0,
+        samples = 0,
+        -- FIXME: IS NOT POSSIBLE USE MAP DICT AS NIL
+        map_dict = map_dict or -1
 
-    end
-    --------------------------------------------------------------
-    function stats.confusion_matrix.oneDatasetIterator(typeDataset)
-        local i = 0
-        local n = typeDataset:numPatterns()
+    }
+    class_instance(obj, self, true)
+    return obj
+end
 
-        return function()
-            i = i+1
+april_set_doc("stats.confusion_matrix.reset", {
+		class = "method", summary = "Reset to 0 all the counters",
+		})
+function stats.confusion_matrix:reset()
 
-            if i <= n then return typeDataset:getPattern(i)[1], typeDataset:getPattern(i)[2] end
-        end
-    end
-
-    function stats.confusion_matrix.twoDatasetsIterator(predDs, gtDs)
-        local i = 0
-        assert(predDs:numPatterns() == gtDs:numPatterns(), "Datasets doesn't have the same size")
-
-        local n = predDs:numPatterns()
-
-        return function()
-            i = i+1
-
-            if i <= n then return predDs:getPattern(i)[1], gtDs:getPattern(i)[1] end
+    for i = 1, self.num_classes do
+        local t = {}
+        for j = 1, self.num_classes do
+            self.confusion[i][j] = 0
         end
     end
-    function stats.confusion_matrix:addData(param1, param2)
+    self.hits = 0
+    self.misses = 0
+    self.samples = 0
+end
 
-        local iterator
-        if( type(param1) == 'function') then
-            iterator = param1
-            assert(param2 == nil)
-        else
-            iterator = stats.confusion_matrix.twoTablesIterator(param1, param2)
-            assert(type(param1) == "table" and type(param2) == "table", "The type of the params is not correct")
-            assert(#param1, #param2, "The tables does not have the same dimension")
+function stats.confusion_matrix:checkType(clase)
+    return type(clase) == "number" and clase >= 1 and clase <= self.num_classes or false
+end
+
+---------------------------------------------
+function stats.confusion_matrix:addSample(pred, gt)
+
+    if self.map_dict ~= -1 then
+        pred = map_dict[pred]
+        gt   = map_dict[gt]
+    end
+    if not self:checkType(pred) or not self:checkType(gt) then
+        error("The class is not correct")
+    end
+
+    if gt == pred then
+        self.hits = self.hits + 1
+    else
+        self.misses = self.misses + 1
+    end
+    self.samples = self.samples + 1
+
+    self.confusion[gt][pred] = self.confusion[gt][pred] + 1
+end
+
+------------------------------------------------
+
+
+april_set_doc("stats.confusion_matrix.printConfusionRaw", {
+		class = "method", summary = "Print the counters for each class",
+		})
+function stats.confusion_matrix:printConfusionRaw()
+
+    for i,v in ipairs(self.confusion) do
+        print(table.concat(v, "\t"))
+    end
+end
+
+april_set_doc("stats.confusion_matrix.printConfusion", {
+		class = "method", summary = "Print the counters for each class and PR and RC",
+    params = { "A num_classes string table [optional] with the tags of each class",}
+		})
+function stats.confusion_matrix:printConfusion(tags)
+
+    printf("\t|\t Predicted ")
+    for i = 1, self.num_classes do
+        printf("\t\t")
+    end
+
+    printf("|\n")
+    printf("______\t|")
+    for i = 1, self.num_classes do
+        printf("\t___\t")
+    end
+
+    printf("\t___\t|\n")
+    for i,v in ipairs(self.confusion) do
+
+        local tag = i
+        if tags then
+            tag = tags[i]
         end
+        printf("%s\t|\t", tag)
+
+        printf("%s\t|\t %0.4f\t|\n", table.concat(v, "\t|\t"), self:getRecall(i))
+    end
+    printf("______\t|")
+    for i = 1, self.num_classes do
+        printf("\t___\t")
+    end
+
+    printf("\t___\t|\n")
+    printf("\t|")
+    for i = 1, self.num_classes do
+        printf("\t%0.4f\t|", self:getPrecision(i))
+    end
+    printf("\t%0.4f\t|\n", self:getError())
+end
+
+--------------------------------------------
+-- Datasets and Tables Iterators
+--------------------------------------------
+function stats.confusion_matrix.twoTablesIterator(table_pred, table_gt)
+    local i = 0
+    local n = #table_pred
+    return function()
+        i = i+1
+        if i <= n then return table_pred[i],table_gt[i] end
+    end
+
+end
+----------------------------------------------------------------
+function stats.confusion_matrix.oneTableIterator(typeTable)
+
+    local i = 0
+    local n = #typeTable
+    return function()
+        i = i+1
+        if i <= n then return typeTable[i][1], typeTable[i][2] end
+    end
+
+end
+--------------------------------------------------------------
+function stats.confusion_matrix.oneDatasetIterator(typeDataset)
+    local i = 0
+    local n = typeDataset:numPatterns()
+
+    return function()
+        i = i+1
+
+        if i <= n then return typeDataset:getPattern(i)[1], typeDataset:getPattern(i)[2] end
+    end
+end
+
+function stats.confusion_matrix.twoDatasetsIterator(predDs, gtDs)
+    local i = 0
+    assert(predDs:numPatterns() == gtDs:numPatterns(), "Datasets doesn't have the same size")
+
+    local n = predDs:numPatterns()
+
+    return function()
+        i = i+1
+
+        if i <= n then return predDs:getPattern(i)[1], gtDs:getPattern(i)[1] end
+    end
+end
+
+---------------------------------------------------------------------------------------------------------
+april_set_doc("stats.confusion_matrix.addData",
+{
+    class = "method",
+    summary = "Add the info of Predicted and Ground Truth set",
+    description = {"This class recieves two tables with the predicted class and the ground truth of that class.",
+    "Also it can recieve a iterator function that returns two elements: predicted sample and groundtruth sample"},
+    params = { "This parameter can be a table of the predicted tags or an iterator function",
+    "This parameter is used if the first parameter is the Predicted table, otherwise it should be nil"},
+})
+function stats.confusion_matrix:addData(param1, param2)
+
+    local iterator
+    if( type(param1) == 'function') then
+        iterator = param1
+        assert(param2 == nil)
+    else
+        iterator = stats.confusion_matrix.twoTablesIterator(param1, param2)
+        assert(type(param1) == "table" and type(param2) == "table", "The type of the params is not correct")
+        assert(#param1, #param2, "The tables does not have the same dimension")
+    end
 
 
-        for pred, gt in iterator do
-            self:addSample(pred, gt)
+    for pred, gt in iterator do
+        self:addSample(pred, gt)
+    end
+end
+
+---------------------------------------------------------------
+april_set_doc("stats.confusion_matrix.getError",
+{
+    class = "method", summary = "Return the global classification error (hits/total)",
+    outputs = { "The global classification error." }, 
+})
+function stats.confusion_matrix:getError()
+    return self.misses/self.samples
+end
+
+april_set_doc("stats.confusion_matrix.getAccuracy", {
+    class = "method", summary = "Return the accuracy (hits/total)",
+    outputs = { "The global accuracy." },
+})
+function stats.confusion_matrix:getAccuracy()
+    return self.hits/self.samples
+end
+
+--------------------------------------------------------------
+function stats.confusion_matrix:getConfusionTables()
+    return self.confusion
+end
+------------------------------------------------------------
+--
+april_set_doc("stats.confusion_matrix.getPrecision",
+{
+    class = "method", summary = "Return the accuracy (hits/total)",
+    params = {"The index of the class for computing the Precision"},
+    outputs = { "The selected class Precision." },
+})
+function stats.confusion_matrix:getPrecision(tipo)
+
+    local tp = 0
+    local den = 0
+
+    -- Moving by columns
+    for i=1, self.num_classes do
+        v = self.confusion[i][tipo]
+        if i == tipo then
+            tp = v
+
         end
+        den = den + v
+    end     
+    if den == 0 then
+        return 0
     end
+    return tp/den
+end
 
-    ---------------------------------------------------------------
-    function stats.confusion_matrix:getError()
-        return self.misses/self.samples
-    end
+april_set_doc("stats.confusion_matrix.getRecall",
+{
+    class = "method", summary = "Return the accuracy (hits/total)",
+    params = {"The index of the class for computing the Recall"},
+    outputs = { "The selected class Recall." },
+})
+function stats.confusion_matrix:getRecall(tipo)
 
-    function stats.confusion_matrix:getAccuracy()
-        return self.hits/self.samples
-    end
+    local tp = 0
+    local den = 0
 
-    --------------------------------------------------------------
-    function stats.confusion_matrix:getConfusionTables()
-        return self.confusion
-    end
-    ------------------------------------------------------------
-    --
-    function stats.confusion_matrix:getPrecision(tipo)
+    -- Moving by columns
+    for j=1, self.num_classes do
+        v = self.confusion[tipo][j]
+        if j == tipo then
+            tp = v
 
-        local tp = 0
-        local den = 0
-
-        -- Moving by columns
-        for i=1, self.num_classes do
-            v = self.confusion[i][tipo]
-            if i == tipo then
-                tp = v
-
-            end
-            den = den + v
-        end     
-        if den == 0 then
-            return 0
         end
-        return tp/den
+        den = den + v
+    end 
+
+    if den == 0 then
+        return 0
     end
+    return tp/den
+end
 
-    function stats.confusion_matrix:getRecall(tipo)
+april_set_doc("stats.confusion_matrix.getFMeasure",
+{
+    class = "method", summary = "Return the accuracy (hits/total)",
+    params = {"The index of the class for computing the Precision"},
+    outputs = { "The selected class Precision." },
+})
+function stats.confusion_matrix:getFMeasure(tipo, beta)
+    local nBeta = beta or 1
+    nBeta = nBeta*nBeta
+    local PR = self:getRecall(tipo)
+    local RC = self:getPrecision(tipo)
 
-        local tp = 0
-        local den = 0
-
-        -- Moving by columns
-        for j=1, self.num_classes do
-            v = self.confusion[tipo][j]
-            if j == tipo then
-                tp = v
-
-            end
-            den = den + v
-        end 
-
-        if den == 0 then
-            return 0
-        end
-        return tp/den
-    end
-
-    function stats.confusion_matrix:getFMeasure(tipo, beta)
-        local nBeta = beta or 1
-        nBeta = nBeta*nBeta
-        local PR = self:getRecall(tipo)
-        local RC = self:getPrecision(tipo)
-
-        return (1+nBeta)*(PR*RC)/(nBeta*PR+RC)
-    end
+    return (1+nBeta)*(PR*RC)/(nBeta*PR+RC)
+end
 
