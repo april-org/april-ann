1b566fcd4d96e73c0a6ea2a3ca512c0f5f1ff9f3
Paco Zamora Martinez <pakozm@gmail.com>
2013-07-11 18:23:28 +0200

    Added templatized functions and functors to reduce the code of matrixFloat.cc

--- packages/basics/matrix/c_src/matrixFloat.cc
+++ packages/basics/matrix/c_src/matrixFloat.cc
@@ -22,58 +22,7 @@
 #include "matrix.h"
 #include "matrixFloat.h"
 #include "matrixFloat_math_templates.h"
-
-///// COMPONENT WISE GENERIC FUNCTOR (NO ARGUMENTS) /////
-/// This class is used as FUNCTOR for the templates located at
-/// matrixFloat_math_templates.h
-template <typename MATRIX, typename Func>
-class component_wise_functor_0 {
-  Func func;
-public:
-  component_wise_functor_0(Func func) : func(func) { }
-  void operator()(MATRIX *m,
-		  unsigned int size, unsigned int stride,
-		  unsigned int offset) const {
-    func(size, m->getRawDataAccess(), stride, offset, m->getCudaFlag());
-  }
-};
-/// This make class is needed to do type inference in template classes
-template <typename Func>
-component_wise_functor_0<MatrixFloat,Func> make_cwise_functor_0(Func f) {
-  return component_wise_functor_0<MatrixFloat, Func>(f);
-}
-template <typename Func>
-component_wise_functor_0<const MatrixFloat,Func>
-make_const_cwise_functor_0(Func f) {
-  return component_wise_functor_0<const MatrixFloat, Func>(f);
-}
-
-///// COMPONENT WISE GENERIC FUNCTOR (ONE ARGUMENTS) /////
-/// This class is used as FUNCTOR for the templates located at
-/// matrixFloat_math_templates.h
-template <typename MATRIX, typename Func>
-class component_wise_functor_1 {
-  float value;
-  Func  func;
-public:
-  component_wise_functor_1(float value,
-			   Func func) : value(value), func(func) { }
-  void operator()(MATRIX *m,
-		  unsigned int size, unsigned int stride,
-		  unsigned int offset) const {
-    func(size, m->getRawDataAccess(), stride, offset, value, m->getCudaFlag());
-  }
-};
-/// This make class is needed to do type inference in template classes
-template <typename Func>
-component_wise_functor_1<MatrixFloat,Func> make_cwise_functor_1(float v, Func f) {
-  return component_wise_functor_1<MatrixFloat,Func>(v, f);
-}
-template <typename Func>
-component_wise_functor_1<const MatrixFloat,Func>
-make_const_cwise_functor_1(float v, Func f) {
-  return component_wise_functor_1<const MatrixFloat,Func>(v, f);
-}
+#include "matrixFloat_math_functors.h"
 
 // WARNING: ALL THE METHODS IMPLEMENTED HERE ARE SPECIALIZED TO FLOAT VERSION
 
