15d65d86e50c6cc2ea83ff28f1c71cf393ad5d2e
Paco Zamora Martinez <pakozm@gmail.com>
2013-08-01 17:40:59 +0200

    Initialized for complex numbers

---
+++ packages/basics/math/c_src/complex_number.h
@@ -0,0 +1,25 @@
+/*
+ * This file is part of the Neural Network modules of the APRIL toolkit (A
+ * Pattern Recognizer In Lua).
+ *
+ * Copyright 2013, Francisco Zamora-Martinez
+ *
+ * The APRIL-MLP toolkit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+#define REAL_IDX 0
+#define IMG_IDX  1
+struct ComplexF {
+  float data[2];
+};
