1df70f4f4f26903a383bc7263cb37fea81aab5d4
Paco Zamora Martinez <pakozm@gmail.com>
2013-09-03 09:18:27 +0200

    Modified lua type function and configure.sh

--- packages/basics/util/lua_src/type.lua
+++ packages/basics/util/lua_src/type.lua
@@ -2,12 +2,5 @@ local luatype = type
 
 function type(x)
     local real_type = luatype(x)
-    if real_type == "userdata" then
-        local t = getmetatable(x)
-        if t ~= nil and t.id ~= nil then
-            return t.id
-        end
-    end
-    return real_type
+    return get_object_id(x) or real_type
 end
-
