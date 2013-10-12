6028480fbaaa884a7308bbf6b82500414049882a
pakozm <pakozm@gmail.com>
2013-10-12 18:39:06 +0200

    Making a shared library with april packages

--- package_list.lua
+++ package_list.lua
@@ -2,43 +2,43 @@ return {
   -- BASIC PACAKGES
   --  "plotter",
   "util",
-  "statistics",
-  "dataset",
-  "matrix",
-  "random",
-  "cmdOpt",
-  "profiler",
-  "math",
-  "gzio",
-  "matlab",
-  "mutual_information",
+  -- "statistics",
+  -- "dataset",
+  -- "matrix",
+  -- "random",
+  -- "cmdOpt",
+  -- "profiler",
+  -- "math",
+  -- "gzio",
+  -- "matlab",
+  -- "mutual_information",
   
-  -- MACHINE LEARNING
-  "trainable",
-  -- "function_interface",
-  -- NEURAL NETWORKS
-  "ann_base",
-  "loss_functions",
-  "trainable",
-  "sdae",
+  -- -- MACHINE LEARNING
+  -- "trainable",
+  -- -- "function_interface",
+  -- -- NEURAL NETWORKS
+  -- "ann_base",
+  -- "loss_functions",
+  -- "trainable",
+  -- "sdae",
 
-  -- LANGUAGE MODELS
-  --  "fnnlm",
+  -- -- LANGUAGE MODELS
+  -- --  "fnnlm",
   
-  -- IMAGE HANDLERS
-  "image",
-  "image_io",
-  "libpng",
-  "median_filter",
-  "image_metrics",
+  -- -- IMAGE HANDLERS
+  -- "image",
+  -- "image_io",
+  -- "libpng",
+  -- "median_filter",
+  -- "image_metrics",
 
-  -- IMAGE PREPROCESSING
-  "binarization_filter",
-  "image_cleaning",
-  "interest_points",
-  --   --"libtiff",
+  -- -- IMAGE PREPROCESSING
+  -- "binarization_filter",
+  -- "image_cleaning",
+  -- "interest_points",
+  -- --   --"libtiff",
   
-  -- HMMs
-  "hmm_trainer",
-  "rates",
+  -- -- HMMs
+  -- "hmm_trainer",
+  -- "rates",
 }
