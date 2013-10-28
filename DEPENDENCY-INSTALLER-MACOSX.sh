7697ac5f30716a82a88f65198f78200981aa1af7
Paco Zamora Martinez <pakozm@gmail.com>
2013-10-28 12:01:30 +0100

    Added dependency installers

--- DEPENDENCY-INSTALLER-MACOSX.sh
+++ DEPENDENCY-INSTALLER-MACOSX.sh
@@ -2,5 +2,9 @@
 port install zlib readline libpng tiff findutils
 echo "This script will change the default system BSD find by GNU find"
 echo "BSD find will be renamed as bfind"
-mv /usr/bin/find /usr/find/bfind
+if [ ! -e /usr/find/bfind ]; then
+    mv /usr/bin/find /usr/find/bfind
+else
+    rm -f /usr/bin/find
+fi
 ln -s `which gfind` /usr/bin/find
