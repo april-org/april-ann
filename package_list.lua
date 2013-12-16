8694d8b2ca37f5ec3c16790dbab456bd72fa5126
pakozm <pakozm@gmail.com>
2013-12-16 19:14:25 +0100

    Added first autodiff version, with only scalars computation.

--- package_list.lua
+++ package_list.lua
@@ -1,4 +1,48 @@
 return { 
-  "autodiff",
-  "rlcompleter",
+  -- BASIC PACAKGES
+  --  "plotter",
+  "util",
+  "stats",
+  "dataset",
+  "matrix",
+  "random",
+  "cmdOpt",
+  "profiler",
+  "mathcore",
+  "gzio",
+  "matlab",
+  "stats.MI",
+  
+  -- MACHINE LEARNING
+  "trainable",
+  -- "function_interface",
+  -- NEURAL NETWORKS
+  "ann",
+  "ann.loss",
+  "trainable",
+  "ann.autoencoders",
+
+  -- LANGUAGE MODELS
+  --  "fnnlm",
+  
+  -- IMAGE HANDLERS
+  "Image",
+  "ImageIO",
+  "libpng",
+  "median_filter",
+  "image_metrics",
+
+  -- IMAGE PREPROCESSING
+  "binarization_filter",
+  "image_cleaning",
+  "interest_points",
+  "image_connected_components",
+  --   --"libtiff",
+  
+  -- HMMs
+  "hmm_trainer",
+  "rates",
+  
+  -- MISC
+  "monad",
 }
