b1560deb7e12cab42f60632ba7ceadcbe3e46a72
Guilherme Defreitas Juraszek <guilherme.defreitas@gmail.com>
2014-01-13 14:03:29 -0200

    Homebrew support addded to install dependencies. The DEPENCENCIES-INSTALLER.sh script must be executed without sudo command

--- DEPENDENCIES-INSTALLER.sh
+++ DEPENDENCIES-INSTALLER.sh
@@ -3,42 +3,47 @@ UNAME=`uname`
 echo "System: $UNAME"
 if [ $UNAME = "Linux" ]; then
     if [ `which apt-get` ]; then
-	apt-get install pkg-config libz-dev libreadline-dev libblas-dev libatlas-dev libpng12-dev libtiff-dev liblua5.2-dev libncurses5 libncurses5-dev ||
-	(echo "ERROR INSTALLING DEPENDENCIES" && exit 10)
-	dest=$(tempfile)
-	echo -e '#include "lua5.2-deb-multiarch.h"\nint main() { return 0; }\n' > $dest.c
-	multiarch=lua/include/lua5.2-deb-multiarch.h
-	multiarch2=lua/lua-5.2.2/src/lua5.2-deb-multiarch.h
-	if ! gcc -c $dest.c 2> /dev/null; then
-	    echo "Not found lua5.2-deb-multiarch.h, generating a default one in $multiarch, please, check the content"
-	    echo -e "#ifndef _LUA_DEB_MULTIARCH_\n#define _LUA_DEB_MULTIARCH_\n#define DEB_HOST_MULTIARCH \""$(arch)"-linux-gnu\"\n#endif" > $multiarch
+  apt-get install pkg-config libz-dev libreadline-dev libblas-dev libatlas-dev libpng12-dev libtiff-dev liblua5.2-dev libncurses5 libncurses5-dev ||
+  (echo "ERROR INSTALLING DEPENDENCIES" && exit 10)
+  dest=$(tempfile)
+  echo -e '#include "lua5.2-deb-multiarch.h"\nint main() { return 0; }\n' > $dest.c
+  multiarch=lua/include/lua5.2-deb-multiarch.h
+  multiarch2=lua/lua-5.2.2/src/lua5.2-deb-multiarch.h
+  if ! gcc -c $dest.c 2> /dev/null; then
+      echo "Not found lua5.2-deb-multiarch.h, generating a default one in $multiarch, please, check the content"
+      echo -e "#ifndef _LUA_DEB_MULTIARCH_\n#define _LUA_DEB_MULTIARCH_\n#define DEB_HOST_MULTIARCH \""$(arch)"-linux-gnu\"\n#endif" > $multiarch
             echo -e "#ifndef _LUA_DEB_MULTIARCH_\n#define _LUA_DEB_MULTIARCH_\n#define DEB_HOST_MULTIARCH \""$(arch)"-linux-gnu\"\n#endif" > $multiarch2
-	fi
-	if [ ! -z $dest ]; then
-	    rm -f $dest*
-	    rm -f $(basename $dest).o
-	fi
+  fi
+  if [ ! -z $dest ]; then
+      rm -f $dest*
+      rm -f $(basename $dest).o
+  fi
     else
-	echo "Error, impossible to install dependencies, this script only works with apt-get"
-	exit 10
+  echo "Error, impossible to install dependencies, this script only works with apt-get"
+  exit 10
     fi
 elif [ $UNAME = "Darwin" ]; then
     if [ `which port` ]; then
-	if ! port install zlib readline libpng tiff findutils pkgconfig lua; then
-	    echo "ERROR INSTALLING DEPENDENCIES"
-	    exit 10
-	fi
-	echo "This script will change the default system BSD find by GNU find"
-	echo "BSD find will be renamed as bfind"
-	if [ ! -e /usr/find/bfind ]; then
-	    mv /usr/bin/find /usr/bin/bfind
-	else
-	    rm -f /usr/bin/find
-	fi
-	ln -s `which gfind` /usr/bin/find
+      if ! port install zlib readline libpng tiff findutils pkgconfig lua; then
+          echo "ERROR INSTALLING DEPENDENCIES"
+          exit 10
+      fi
+      echo "This script will change the default system BSD find by GNU find"
+      echo "BSD find will be renamed as bfind"
+      if [ ! -e /usr/find/bfind ]; then
+        mv /usr/bin/find /usr/bin/bfind
+      else
+        rm -f /usr/bin/find
+      fi
+      ln -s `which gfind` /usr/bin/find
+    if [ `which brew` ]; then
+      if ! brew install lzlib readline libpng libtiff findutils pkgconfig lua; then
+          echo "ERROR INSTALLING DEPENDENCIES USING HOMEBREW"
+          exit 10
+      fi
     else
-	echo "Error, impossible to install dependencies, this scripts needs MacPorts"
-	exit 10
+      echo "Error, impossible to install dependencies, this scripts needs MacPorts or Homebrew"
+      exit 10
     fi
 else
     echo "Error, impossible to install dependencies, not recognized system: $UNAME"
