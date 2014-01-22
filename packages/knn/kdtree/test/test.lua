23c4325c8bcbcba12a600d6c3a9cedef49cef6c2
Paco Zamora Martinez <pakozm@gmail.com>
2014-01-22 11:26:22 +0100

    Modified test for kdtree

--- packages/knn/kdtree/test/test.lua
+++ packages/knn/kdtree/test/test.lua
@@ -1,24 +1,32 @@
-rnd = random(1234)
-m1  = matrix(10,4):uniformf(0,1,rnd)
-m2  = matrix(10,4):uniformf(0,1,rnd)
-print(m1)
-print(m2)
-kdt = knn.kdtree(m1:dim(2),rnd)
+local N   = 1000000
+local D   = 100
+local rnd = random(1234)
+local m1  = matrix(N,D):uniformf(0,1,rnd)
+local m2  = matrix(N,D):uniformf(0,1,rnd)
+--
+local kdt = knn.kdtree(m1:dim(2),rnd)
 kdt:push(m1)
 kdt:push(m2)
 kdt:build()
-kdt:print()
-p = matrix(1,4):uniformf(0,1,rnd)
-bestid,bestdist,best = kdt:searchNN(p)
+-- KDTREE SEARCH
+local p = matrix(1,D):uniformf(0,1,rnd)
+local bestid,bestdist,best = kdt:searchNN(p)
 print(p)
 print(bestid)
 print(bestdist)
-
+-- NAIVE LINEAR SEARCH
+local besti
+local bestd = math.huge
 for i=1,m1:dim(1) do
   local aux = m1(i,':')
-  print(i, (aux-p):pow(2):sum())
+  local d = (aux-p):pow(2):sum()
+  if d < bestd then bestd,besti = d,i end
 end
 for i=1,m2:dim(1) do
   local aux = m2(i,':')
-  print(m1:dim(1)+i, (aux-p):pow(2):sum())
+  local d = (aux-p):pow(2):sum()
+  local j = m1:dim(1)+i
+  if d < bestd then bestd,besti = d,j end
 end
+print(besti)
+print(bestd)
