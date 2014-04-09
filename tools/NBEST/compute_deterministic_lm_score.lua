415dabb359e09162951d2a8d2c4a90c4b64a5969
Baha <ukonat@gmail.com>
2014-04-09 02:44:09 +0200

    Adapted scoring script to new interface.

--- tools/NBEST/compute_deterministic_lm_score.lua
+++ tools/NBEST/compute_deterministic_lm_score.lua
@@ -29,8 +29,9 @@ collectgarbage("collect")
 -- cargamos el modelo FAST
 lm = ngram.load_language_model(lm_file, vocab, begin_word, end_word,
 			       {trie_size=trie_size})
+local lmi = lm:get_interface()
 collectgarbage("collect")
-use_cache = lm:has_cache()
+--use_cache = lm:has_cache()
 
 ------------------------------------- GO!
 
@@ -43,37 +44,48 @@ local prevn = nil
 lm:restart()
 nbestf = io.open(nbestfile, "r")
 cachef = nil
-if cache_extra_set then
-  cachef = io.open(cache_extra_set, "r")
-end
-local last_best_line = nil
+--if cache_extra_set then
+--  cachef = io.open(cache_extra_set, "r")
+--end
+--local last_best_line = nil
+
+assert(lm:is_deterministic(),
+       "Error: Expected a deterministic LM")
+
 while true do
   local line = nbestf:read("*l")
   local n
   if line then n = tonumber(string.match(line, "(.*)|||.*|||.*|||")) + 1 end
   if not prevn then prevn = n last_best_line = line end
   if not line or n ~= prevn then
+    --[[
     if cachef then
       local line  = cachef:read("*l")
       local words = string.tokenize(line)
       for i=1,#words do
-	if words[i] == stop_word then lm:clearCache()
-	elseif words[i] ~= null_word then
-	  local w = dictionary:getWordId(words[i]) or dictionary:getWordId(unk_word)
-	  ngram_model:cacheWord(w)
-	end
+        if words[i] == stop_word then lm:clearCache()
+        elseif words[i] ~= null_word then
+	        local w = dictionary:getWordId(words[i]) or dictionary:getWordId(unk_word)
+          ngram_model:cacheWord(w)
+        end
       end
     end
+    --]]
     -- compute scores for each collected ngram
     for key,where in pairs(ngrams) do
-      lm:prepare(key)
+      lmi:clear_queries()
       local sum=0
-      for word,d in pairs(where) do
-	local p = lm:get(key, word)
-	for _,id in ipairs(d) do
-	  scores[id] = scores[id] + p
-	end
-	sum = sum + #d
+      for word,_ in pairs(where) do
+        lmi:insert_query(key, word)
+      end
+      result = get_queries()
+      local i = 1
+      for word,d in pairs(where)
+        _,p = result:get(i)
+        for _,id in ipairs(d) do
+          scores[id] = scores[id] + p
+        end
+        sum = sum + #d
       end
       --fprintf(io.stderr, "%d : %d\n", key, sum)
     end
@@ -81,12 +93,14 @@ while true do
     scores    = {}
     ngrams    = {}
     prevn     = n
+    --[[
     if use_cache or n and math.mod(n, 100) == 99 then lm:restart() end
     if use_cache and last_best_line then
       local words = string.tokenize(string.match(last_best_line, "|||(.*)|||.*|||"))
       local wids  = vocab:searchWordIdSequence(words, vocab:getWordId(unk_word))
       for i=1,#wids do lm:cacheWord(wids[i]) end
     end
+    --]]
     last_best_line = line
   end
   if not line then break end
@@ -100,15 +114,15 @@ while true do
   local context = {}
   local key
   if use_bcc then
-    for i=1,lm:getLMOrder()-1 do
+    for i=1,lm:ngram_order()-1 do
       table.insert(context, vocab:getWordId(begin_word))
     end
-    key = lm:getInitialKey(vocab:getWordId(begin_word))
+    key = lmi:get_initial_key()
   else
-    for i=1,lm:getLMOrder()-1 do
+    for i=1,lm:ngram_order()-1 do
       table.insert(context, 0)
     end
-    key = lm:getZeroGramKey(vocab:getWordId(begin_word))
+    key = lmi:get_zero_key()
   end
   for wpos=1,#wids do
     ngrams[key] = ngrams[key] or {}
@@ -119,7 +133,9 @@ while true do
     table.remove(context, 1)
     table.insert(context, w)
     -- next key
-    key = lm:findKeyFromNgram(context)
+    result = lmi:next_keys(key, w)
+    assert(#result == 1)
+    key = result:get(1)
   end
   if use_ecc then
     ngrams[key] = ngrams[key] or {}
