7a1159f0c39b03f0e3d40ed0f14ca3042bba69e4
Baha <ukonat@gmail.com>
2014-04-22 18:47:55 +0200

    Declared interfaces for new models.

--- packages/language_model/LM_interface/c_src/LM_interface.h
+++ packages/language_model/LM_interface/c_src/LM_interface.h
@@ -62,7 +62,7 @@ namespace LanguageModels {
       Burden  burden;
       KeyScoreBurdenTuple() {}
       KeyScoreBurdenTuple(Key k, Score s, Burden burden) :
-	key_score(k,s), burden(burden) {}
+        key_score(k,s), burden(burden) {}
     };
     
     /// This struct is used for the insertQuery method which receives several
@@ -73,7 +73,7 @@ namespace LanguageModels {
       Score    score;
       WordIdScoreTuple() {}
       WordIdScoreTuple(WordType w, int32_t idw, Score s) :
-	word(w), id_word(idw), score(s) {}
+        word(w), id_word(idw), score(s) {}
     };
     
   private:
@@ -100,19 +100,19 @@ namespace LanguageModels {
     /// the bunch (multiple queries) mode
     /// TODO: threshold should have a default value
     virtual void get(const Key &key, WordType word, Burden burden,
-		     vector<KeyScoreBurdenTuple> &result,
-		     Score threshold) = 0;
+                     vector<KeyScoreBurdenTuple> &result,
+                     Score threshold) = 0;
     
     /// this method computes the next keys given a pair (key,word). It could be a
     /// non-deterministic LM. By default, it uses the standard get() method and
     /// discards the Burden and Score.
     virtual void getNextKeys(const Key &key, WordType word,
-			     vector<Key> &result) {
+                             vector<Key> &result) {
       vector<KeyScoreBurdenTuple> aux_result;
       get(key, word, Burden(-1,-1), aux_result, Score::zero());
       for (typename vector<KeyScoreBurdenTuple>::iterator it = aux_result.begin();
-	   it != aux_result.end(); ++it)
-	result.push_back(it->key_score.key);
+           it != aux_result.end(); ++it)
+        result.push_back(it->key_score.key);
     }
     
     // -------------- BUNCH MODE -------------
@@ -130,15 +130,15 @@ namespace LanguageModels {
     /// call this method for each individual query.
     /// The default implementation use the get method
     virtual void insertQuery(const Key &key, WordType word, Burden burden,
-			     Score threshold) {
+                             Score threshold) {
       get(key,word,burden,result,threshold);
     }
     
     /// this method can be naively converted into a series of
     /// insertQuery calls, but it can be optimized for some 
     virtual void insertQueries(const Key &key, int32_t id_key,
-			       vector<WordIdScoreTuple> words,
-			       bool is_sorted=false) {
+                               vector<WordIdScoreTuple> words,
+                               bool is_sorted=false) {
       UNUSED_VARIABLE(is_sorted);
       // default behavior
       for (typename vector<WordIdScoreTuple>::iterator it = words.begin();
@@ -186,6 +186,37 @@ namespace LanguageModels {
     // technique (in the same way as with get method):
     virtual Score getFinalScore(const Key &k, Score threshold) = 0;
   };
+
+  template <typename Key, typename Score>
+  class HistoryBasedLMInterface : public LMInterface <Key,Score> {
+  private:
+    LMModel<Key,Score>* model;
+    
+  public:
+
+    HistoryBasedLMInterface(LMModel<Key,Score>* model) :
+      model(model) {
+      IncRef(model);
+    }
+
+    ~HistoryBasedLMInterface() {
+      DecRef(model);
+    }
+    
+    virtual bool getZeroKey(Key &k) const {
+      return true;
+    }
+
+    virtual void getInitialKey(Key &k) const {
+      ;
+    }
+
+    virtual void get(const Key &key, WordType word,
+                     typename LMInterface<Key,Score>::Burden burden,
+                     vector<typename LMInterface<Key,Score>::KeyScoreBurdenTuple> &result,
+                     Score threshold) {
+    }
+  };
   
   /// The LMModel is the thread-safe part of the LM, where the model data is
   /// stored
@@ -220,27 +251,75 @@ namespace LanguageModels {
   };
 
   template <typename Key, typename Score>
-  class HistoryBasedLM : public LMInterface<Key,Score> {
+  class HistoryBasedLM : public LMModel <Key,Score> {
   private:
+    int ngram_order;
+    WordType init_word;
     april_utils::TrieVector *trie_vector;
+
   public:
-    HistoryBasedLM(april_utils::TrieVector *trie_vector) : 
+
+    HistoryBasedLM(int ngram_order,
+                   WordType init_word,
+                   april_utils::TrieVector *trie_vector) : 
+      ngram_order(ngram_order),             
+      init_word(init_word),             
       trie_vector(trie_vector) {
       IncRef(trie_vector);
     }
+
     ~HistoryBasedLM() {
       DecRef(trie_vector);
     }
+
+    virtual bool isDeterministic() const {
+      return true;
+    }
+    
+    virtual int ngramOrder() const {
+      return ngram_order;
+    }
+
+    virtual bool requireHistoryManager() const {
+      return true;
+    }
+
+    virtual LMInterface<Key,Score>* getInterface() {
+      return new HistoryBasedLMInterface<Key,Score>(this);
+    }
   };
-  
+
   template <typename Key, typename Score>
-  class BunchHashedLM : public LMInterface<Key,Score> {
+  class BunchHashedLM : public LMModel <Key,Score> {
   private:
+    int ngram_order;
     unsigned int bunch_size;
+
   public:
-    BunchHashedLM(unsigned int bunch_size) :
+
+    BunchHashedLM(int ngram_order,
+                  unsigned int bunch_size) :
+      ngram_order(ngram_order),
       bunch_size(bunch_size) { }
+
+    virtual bool isDeterministic() const {
+      return true;
+    }
+    
+    virtual int ngramOrder() const {
+      return ngram_order;
+    }
+
+    virtual bool requireHistoryManager() const {
+      return true;
+    }
+
+    virtual LMInterface<Key,Score>* getInterface() {
+      return 0;
+      //return new BunchHashedLMInterface<Key,Score>(this);
+    }
     unsigned int getBunchSize() { return bunch_size; }
+
     void setBunchSize(unsigned int bunch_size) {
       this->bunch_size = bunch_size;
     }
