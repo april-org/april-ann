7b9e2c4e04005449fbecbe81dd3046f79c2525ad
Paco Zamora Martinez <pakozm@gmail.com>
2014-05-22 09:17:21 +0200

    Updated travis for macosx

--- .maketravis.sh
+++ .maketravis.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 . configure.sh
-if [[ $CC == gcc ]]; then
-    echo "**** GCC RELEASE-ATLAS ****"     &&
-    make release-atlas                     &&
-    echo "**** GCC TEST-DEBUG-ATLAS ****"  &&
-    make test-debug-atlas                  &&
-    echo "**** GCC RELEASE-NO-OMP ****"    &&
-    make release-no-omp                    &&
-    echo "**** GCC TEST-DEBUG-NO-OMP ****" &&
-    make test-debug-no-omp
-elif [[ $CC == clang ]]; then
-    echo "**** CLANG RELEASE-NO-OMP ****"     &&
-    make release-no-omp                       &&
-    echo "**** CLANG TEST-DEBUG-NO-OMP ****"  &&
-    make test-debug-no-omp
+UNAME=$(uname)
+if [[ $UNAME == "Linux" ]]; then
+    if [[ $CC == gcc ]]; then
+        ## echo "**** GCC RELEASE-ATLAS ****"     &&
+        ## make release-atlas                     &&
+        echo "**** GCC TEST-DEBUG-ATLAS ****"  &&
+        make test-debug-atlas                  &&
+        ## echo "**** GCC RELEASE-NO-OMP ****"    &&
+        ## make release-no-omp                    &&
+        echo "**** GCC TEST-DEBUG-NO-OMP ****" &&
+        make test-debug-no-omp
+    elif [[ $CC == clang ]]; then
+        ## echo "**** CLANG RELEASE-NO-OMP ****"     &&
+        ## make release-no-omp                       &&
+        echo "**** CLANG TEST-DEBUG-NO-OMP ****"  &&
+        make test-debug-no-omp
+    else
+        echo "Unknown variable CC=$CC"
+        exit 10
+    fi
+elif [[ $UNAME == "Darwin" ]]; then
+    if [[ $CC == clang ]]; then
+        ## echo "**** CLANG RELEASE-MACOSX ****"     &&
+        ## make release-macosx                       &&
+        echo "**** CLANG TEST-DEBUG-MACOSX ****"  &&
+        make test-debug-macosx
+    else
+        echo "Unknown variable CC=$CC"
+        exit 10
+    fi
 else
-    echo "Unknown variable CC=$CC"
-    exit -1
+    echo "Unknown UNAME=$UNAME"
+    exit 10
 fi
