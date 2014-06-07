9550f8033b7fc385daffe7dbe8e460d4027323a3
Paco Zamora Martinez <pakozm@gmail.com>
2014-06-07 13:45:02 +0200

    Added license disclaimer in source files of LM interface

--- packages/language_model/ngram_lira/c_src/arpa2lira.h
+++ packages/language_model/ngram_lira/c_src/arpa2lira.h
@@ -1,3 +1,24 @@
+/*
+ * This file is part of APRIL-ANN toolkit (A
+ * Pattern Recognizer In Lua with Artificial Neural Networks).
+ *
+ * Copyright 2012, Salvador España-Boquera, Adrian Palacios Corella, Francisco
+ * Zamora-Martinez
+ *
+ * The APRIL-ANN toolkit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
 #ifndef ARPA2LIRA_H
 #define ARPA2LIRA_H
 
