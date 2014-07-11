3a212d151fbf26dbaec9065ecc0bfb03e273e81d
Baha <ukonat@gmail.com>
2014-07-11 03:34:52 +0200

    Added code for BunchHashed wrapper. Not tested.

---
+++ packages/language_model/ngram_lira/c_src/bunch_hashed_ngram_lira.cc
@@ -0,0 +1,75 @@
+/*
+ * This file is part of APRIL-ANN toolkit (A
+ * Pattern Recognizer In Lua with Artificial Neural Networks).
+ *
+ * Copyright 2012, Salvador España-Boquera, Adrian Palacios Corella, Francisco
+ * Zamora-Martinez
+ *
+ * The APRIL-ANN toolkit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+#include "bunch_hashed_ngram_lira.h"
+#include "unused_variable.h"
+
+namespace LanguageModels {
+  
+  typedef NgramLiraModel::Key Key;
+  typedef NgramLiraModel::Score Score;
+
+  BunchHashedNgramLiraLMInterface::
+  BunchHashedNgramLiraLMInterface(BunchHashedNgramLiraLM *model,
+				   NgramLiraModel *lira_model) :
+    BunchHashedLMInterface(model) {
+    lira_interface = 
+      static_cast<NgramLiraInterface*>(lira_model->getInterface());
+    IncRef(lira_interface);
+  }
+
+  BunchHashedNgramLiraLMInterface::
+  ~BunchHashedNgramLiraLMInterface() {
+    DecRef(lira_interface);
+  }
+
+  Score BunchHashedNgramLiraLMInterface::
+  getBestProb() const {
+    return lira_interface->getBestProb();
+  }
+
+  void BunchHashedNgramLiraLMInterface::
+  computeKeysAndScores(KeyWordHash &ctxt_hash,
+                       unsigned int bunch_size) {
+    
+  }
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  
+  BunchHashedNgramLiraLM::
+  BunchHashedNgramLiraLM(unsigned int bunch_size,
+                         NgramLiraModel *lira_model) :
+    BunchHashedLM(lira_model->ngramOrder(), bunch_size),
+    lira_model(lira_model) {
+    IncRef(lira_model);
+  }
+
+  BunchHashedNgramLiraLM::~BunchHashedNgramLiraLM() {
+    DecRef(lira_model);
+  }
+  
+  LMInterface<Key,Score>* BunchHashedNgramLiraLM::getInterface() {
+    return new BunchHashedNgramLiraLMInterface(this, lira_model);
+  }
+  
+} // closes namespace language_models
---
+++ packages/language_model/ngram_lira/c_src/bunch_hashed_ngram_lira.h
@@ -0,0 +1,76 @@
+/*
+ * This file is part of APRIL-ANN toolkit (A
+ * Pattern Recognizer In Lua with Artificial Neural Networks).
+ *
+ * Copyright 2012, Salvador España-Boquera, Adrian Palacios Corella, Francisco
+ * Zamora-Martinez
+ *
+ * The APRIL-ANN toolkit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+#ifndef HISTORY_BASED_NGRAM_LIRA_H
+#define HISTORY_BASED_NGRAM_LIRA_H
+
+#include "bunch_hashed_LM.h"
+#include "ngram_lira.h"
+
+namespace LanguageModels {
+
+  class BunchHashedNgramLiraLM;
+  
+  class BunchHashedNgramLiraLMInterface :
+    public BunchHashedLMInterface< NgramLiraModel::Key,
+                                   NgramLiraModel::Score > {
+  public:
+    typedef NgramLiraModel::Key Key;
+    typedef NgramLiraModel::Score Score;
+    
+    virtual ~BunchHashedNgramLiraLMInterface();
+    Score getBestProb() const;
+
+  protected:
+    friend class BunchHashedNgramLiraLM;
+    BunchHashedNgramLiraLMInterface(BunchHashedNgramLiraLM *model,
+				     NgramLiraModel *lira_model);
+
+    virtual void computeKeysAndScores(KeyWordHash &ctxt_hash,
+                                      unsigned int bunch_size);
+  private:
+
+    NgramLiraInterface *lira_interface;
+  
+  };
+
+  class BunchHashedNgramLiraLM : public BunchHashedLM<NgramLiraModel::Key,
+                                                      NgramLiraModel::Score > {
+  public:
+    typedef NgramLiraModel::Key Key;
+    typedef NgramLiraModel::Score Score;
+
+  private:
+    NgramLiraModel *lira_model;
+
+  public:
+    BunchHashedNgramLiraLM(unsigned int bunch_size,
+                           NgramLiraModel *lira_model);
+
+    virtual ~BunchHashedNgramLiraLM();
+
+    virtual LMInterface<Key,Score>* getInterface();
+  };
+
+} // closes namespace language_models
+
+#endif // HISTORY_BASED_NGRAM_LIRA_H
