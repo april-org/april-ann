d1ddd2d5e88b75474c719db851dcc252be36901f
Paco Zamora Martinez <pakozm@gmail.com>
2014-07-31 14:58:45 +0200

    added zipio package

---
+++ packages/misc/zipio/package.lua
@@ -0,0 +1,58 @@
+ package{ name = "gzio",
+   version = "1.0",
+   depends = { "util", "tar", "april_io" },
+   link_libraries = { "z" },
+   keywords = { "gzio" },
+   description = "read and write gz files",
+   -- targets como en ant
+   target{
+     name = "init",
+     mkdir{ dir = "build" },
+     mkdir{ dir = "include" },
+   },
+   target{ name = "clean",
+     delete{ dir = "build" },
+     delete{ dir = "include" },
+   },
+   target{
+     name = "test",
+     execute_script{
+       file={
+	 "test/test.lua",
+       },
+     },
+   },
+   target{
+     name = "provide",
+     depends = "init",
+     copy{ file= "c_src/*.h", dest_dir = "include" },
+     provide_bind{ file = "binding/bind_gzio.lua.cc", dest_dir = "include" }
+   },
+   target{
+     name = "build",
+     depends = "provide",
+     use_timestamp = true,
+     object{ 
+       file = "c_src/*.cc",
+       include_dirs = "${include_dirs}",
+       dest_dir = "build",
+     },
+     luac{
+       orig_dir = "lua_src",
+       dest_dir = "build",
+     },
+     build_bind{
+        file = "binding/bind_gzio.lua.cc",
+        dest_dir = "build",
+     }
+   },
+   target{
+     name = "document",
+     document_src{
+     },
+     document_bind{
+     },
+   },
+ }
+ 
+ 
