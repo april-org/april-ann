71fad48e19a2206994fcaf70c848d34731f4695a
Paco Zamora Martinez <pakozm@gmail.com>
2014-10-18 18:29:34 +0200

    Improved print in matrix

--- packages/basics/matrix/lua_src/generic_print.lua
+++ packages/basics/matrix/lua_src/generic_print.lua
@@ -4,31 +4,31 @@ matrix.__generic__ = matrix.__generic__ or {}
 matrix.__generic__.__make_generic_print__ = function(name,getter)
   assert(name and getter)
   return function(self)
-    local MAX    = 10
-    local dims   = self:dim()
-    local major  = (self.get_major_order and self:get_major_order()) or "row_major"
-    local coords = {}
-    local out    = {}
-    local row    = {}
-    local so_large = false
-    for i=1,#dims do 
-      coords[i]=1
-      if dims[i] > MAX then so_large = true end
-    end
-    -- if not so_large then
+    local MAX_ROW_LEN,MAX_ROWS = 60,6
+    local dims     = self:dim()
+    local major    = (self.get_major_order and self:get_major_order()) or "row_major"
+    local coords   = {}
+    local out      = {}
+    local row      = {}
+    local row_len  = 0
+    for i=1,#dims do coords[i]=1 end
     for i=1,self:size() do
       if #dims > 2 and coords[#dims] == 1 and coords[#dims-1] == 1 then
         table.insert(out,
                      string.format("\n# pos [%s]",
                                    table.concat(coords, ",")))
       end
-      if not coords[#coords-1] or coords[#coords-1] < MAX or coords[#coords-1] == dims[#coords-1] then
-        if #row < MAX or coords[#coords] == dims[#coords] then
-          table.insert(row, getter(self:get(table.unpack(coords))))
-        elseif #row == MAX then
-          table.insert(row, "...")
+      if not coords[#coords-1] or coords[#coords-1] < MAX_ROWS or coords[#coords-1] == dims[#coords-1] then
+        if row_len < MAX_ROW_LEN or coords[#coords] == dims[#coords] then
+          local str = getter(self:get(table.unpack(coords)))
+          row_len = row_len + #str + 1
+          if row_len < MAX_ROW_LEN  or coords[#coords] == dims[#coords] then
+            table.insert(row, str)
+          else
+            table.insert(row, "...")
+          end
         end
-      elseif coords[#coords-1] == MAX then
+      elseif coords[#coords-1] == MAX_ROWS then
         row = { "..." }
       end
       local j=#dims+1
@@ -38,12 +38,9 @@ matrix.__generic__.__make_generic_print__ = function(name,getter)
         if coords[j] > dims[j] then coords[j] = 1 end
       until j==1 or coords[j] ~= 1
       if coords[#coords] == 1 then
-        if #row > 0 then table.insert(out, table.concat(row, " ")) row={} end
+        if #row > 0 then table.insert(out, table.concat(row, " ")) row={} row_len = 0 end
       end
     end
-    -- else
-    -- table.insert(out, "Large matrix, not printed to display")
-    -- end
     table.insert(out, string.format("# %s of size [%s] in %s [%s]\n",
 				    name,
 				    table.concat(dims, ","), major,
