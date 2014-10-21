c18e81324fb6023eaff7f9f0edc7b5f73e48d893
Paco Zamora Martinez <pakozm@gmail.com>
2014-10-21 10:28:10 +0200

    Solved bug at transpose

--- packages/basics/matrix/c_src/matrix.impl.h
+++ packages/basics/matrix/c_src/matrix.impl.h
@@ -503,6 +503,7 @@ namespace Basics {
         result->stride[j]     = this->stride[i];
         result->matrixSize[j] = this->matrixSize[i];
       }
+      result->is_contiguous = NONE;
     }
     else result = this;
     return result;
