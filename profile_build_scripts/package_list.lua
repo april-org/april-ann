0f43c9f82f5f7c03fe6f2e3548c1e1838ede8f7f
Paco Zamora Martinez <pakozm@gmail.com>
2014-12-30 19:03:23 +0100

    Uncommenting packages

--- profile_build_scripts/package_list.lua
+++ profile_build_scripts/package_list.lua
@@ -9,62 +9,62 @@ return {
   "stats",
   "dataset",
   "matrix",
-  -- "random",
-  -- "cmdOpt",
-  -- "complex",
-  -- "mathcore",
-  -- "matlab",
-  -- "stats.MI",
-  -- "autodiff",
-  -- "autodiff.ann",
+  "random",
+  "cmdOpt",
+  "complex",
+  "mathcore",
+  "matlab",
+  "stats.MI",
+  "autodiff",
+  "autodiff.ann",
 
-  -- -- INPUT/OUTPUT
-  -- "aprilio",
-  -- "gzio",
-  -- "zip",
-  -- "tar",
+  -- INPUT/OUTPUT
+  "aprilio",
+  "gzio",
+  "zip",
+  "tar",
   
-  -- -- MACHINE LEARNING
-  -- "clustering.kmeans.matrix",
-  -- "knn.kdtree",
-  -- "trainable",
-  -- -- "function_interface",
+  -- MACHINE LEARNING
+  "clustering.kmeans.matrix",
+  "knn.kdtree",
+  "trainable",
+  -- "function_interface",
   
-  -- -- NEURAL NETWORKS
-  -- "ann",
-  -- "ann.loss",
-  -- "trainable",
-  -- "ann.autoencoders",
-  -- "ann.graph",
-  -- "bayesian",
+  -- NEURAL NETWORKS
+  "ann",
+  "ann.loss",
+  "trainable",
+  "ann.autoencoders",
+  "ann.graph",
+  "bayesian",
 
-  -- -- LANGUAGE MODELS
-  -- "language_models",
-  -- "ngram.lira",
+  -- LANGUAGE MODELS
+  "language_models",
+  "ngram.lira",
   
-  -- -- IMAGE HANDLERS
-  -- "Image",
-  -- "ImageIO",
-  -- "libpng",
-  -- "median_filter",
-  -- "image_metrics",
+  -- IMAGE HANDLERS
+  "Image",
+  "ImageIO",
+  "libpng",
+  "median_filter",
+  "image_metrics",
 
-  -- -- IMAGE PREPROCESSING
-  -- "binarization_filter",
-  -- "image_cleaning",
-  -- "interest_points",
-  -- "image_connected_components",
-  -- --   --"libtiff",
-  -- "ocr.off_line.param",
-  -- "ocr.off_line_text_preprocessing",
+  -- IMAGE PREPROCESSING
+  "binarization_filter",
+  "image_cleaning",
+  "interest_points",
+  "image_connected_components",
+  --   --"libtiff",
+  "ocr.off_line.param",
+  "ocr.off_line_text_preprocessing",
   
-  -- -- HMMs
-  -- "hmm_trainer",
+  -- HMMs
+  "hmm_trainer",
   
-  -- -- Metrics
-  -- "metrics.rates",
-  -- "metrics.roc",
+  -- Metrics
+  "metrics.rates",
+  "metrics.roc",
 
-  -- -- MISC
-  -- "profiler",
+  -- MISC
+  "profiler",
 }
