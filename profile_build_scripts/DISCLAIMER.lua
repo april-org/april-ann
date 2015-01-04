c1227e3ce12718135f77a03f32215c8bfeab5176
Paco Zamora Martinez <pakozm@gmail.com>
2015-01-04 11:53:51 +0100

    Updated license year

--- profile_build_scripts/DISCLAIMER.lua
+++ profile_build_scripts/DISCLAIMER.lua
@@ -1,4 +1,4 @@
 return {
-  '"APRIL-ANN v" APRILANN_VERSION_MAJOR "." APRILANN_VERSION_MINOR "." APRILANN_VERSION_RELEASE " COMMIT " TOSTRING(GIT_COMMIT) "  Copyright (C) 2012-2014 DSIC-UPV, CEU-UCH"',
+  '"APRIL-ANN v" APRILANN_VERSION_MAJOR "." APRILANN_VERSION_MINOR "." APRILANN_VERSION_RELEASE " COMMIT " TOSTRING(GIT_COMMIT) "  Copyright (C) 2012-2015 DSIC-UPV, CEU-UCH"',
   '"This program comes with ABSOLUTELY NO WARRANTY; for details see LICENSE.txt.\\nThis is free software, and you are welcome to redistribute it\\nunder certain conditions; see LICENSE.txt for details."',
 }
