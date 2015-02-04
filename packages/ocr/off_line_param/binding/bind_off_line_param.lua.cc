1d0ba2460f427f6dc45801d1e87c36353453d76a
Joan Pastor <joapaspe@gmail.com>
2015-02-04 15:13:22 +0100

    Baselines RLSA binding and SVG line added

--- packages/ocr/off_line_param/binding/bind_off_line_param.lua.cc
+++ packages/ocr/off_line_param/binding/bind_off_line_param.lua.cc
@@ -36,3 +36,17 @@ using namespace Imaging;
   // LUABIND_RETURN(ImageFloat,img);
 }
 //BIND_END
+
+//BIND_FUNCTION ocr.off_line.baselinesRLSA
+{
+  ImageFloat *img;
+  
+  float threshold;
+  int upper, lower;
+  LUABIND_GET_PARAMETER(1, ImageFloat, img);
+  LUABIND_GET_OPTIONAL_PARAMETER(2, float, threshold, 0.5f);
+  OCR::OffLineTextPreprocessing::baseLinesRLSA(img, &lower, &upper, threshold);
+  LUABIND_RETURN(int, lower);
+  LUABIND_RETURN(int, upper);
+}
+//BIND_END
