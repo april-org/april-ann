ba40d36be600217cefd4c13e59b035ee5a574231
Paco Zamora Martinez <pakozm@gmail.com>
2015-05-01 12:28:24 +0200

    Minor changes in matrix

--- packages/basics/matrix/c_src/matrix-serialization.impl.h
+++ packages/basics/matrix/c_src/matrix-serialization.impl.h
@@ -202,7 +202,8 @@ namespace Basics {
     if (is_ascii) sizedata = ascii_sizer(this);
     else sizedata = bin_sizer(this);
     // size_t expected_size = static_cast<size_t>(sizedata+sizeheader+1);
-    // UNUSED_VARIABLE(expected_size);
+    UNUSED_VARIABLE(sizedata);
+    UNUSED_VARIABLE(sizeheader);
     if (!stream->isOpened()) {
       ERROR_EXIT(256, "The stream is not prepared\n");
     }
