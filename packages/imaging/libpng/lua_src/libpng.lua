2790acf2ca4c60a26b3880c70f536ccdc975db7e
Paco Zamora Martinez <pakozm@gmail.com>
2015-10-08 19:27:17 +0200

    Supportting IPyLua using metamethod ipylua_show

--- packages/imaging/libpng/lua_src/libpng.lua
+++ packages/imaging/libpng/lua_src/libpng.lua
@@ -1,23 +1,25 @@
 ImageIO.handlers["png"] = { read=libpng.read, write=libpng.write }
 
 -- support for IPyLua
-do
-  local handlers = debug.getregistry().APRILANN.IPyLua_output_handlers
-
-  handlers[ ImageRGB ] = function(obj)
-    local data = {
-      ["text/plain"] = tostring(obj),
-      ["image/png"] = ee5_base64.encode(libpng.write(obj)),
-    }
-    local w,h = obj:geometry()
-    local metadata = {
-      ["image/png"] = { width = w, height = h }
-    }
-    return data,metadata
-  end
-  
-  handlers[ Image ] = function(obj)
-    local obj = obj:to_RGB()
-    return handlers[ class.of(obj) ]( obj )
-  end
+local function ImageRGB_show(obj)
+  local data = {
+    ["text/plain"] = tostring(obj),
+    ["image/png"] = ee5_base64.encode(libpng.write(obj)),
+  }
+  local w,h = obj:geometry()
+  local metadata = {
+    ["image/png"] = { width = w, height = h }
+  }
+  return data,metadata
 end
+
+-- support for IPyLua
+class.extend_metamethod(ImageRGB, "ipylua_show", ImageRGB_show)
+
+local function Image_show(obj)
+  local obj = obj:to_RGB()
+  return ImageRGB_show(obj)
+end
+
+-- support for IPyLua
+class.extend_metamethod(Image, "ipylua_show", Image_show)
