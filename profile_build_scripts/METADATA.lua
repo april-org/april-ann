cac229ecd3504ae296baded36de2fd406ca03057
Paco Zamora Martinez <pakozm@gmail.com>
2015-12-03 13:02:59 +0100

    Version 0.4.1

--- profile_build_scripts/METADATA.lua
+++ profile_build_scripts/METADATA.lua
@@ -1,12 +1,12 @@
 return {
   description = "A Pattern Recognizer In Lua",
-  version = "0.4.0",
+  version = "0.4.1",
   prefix = os.getenv("PREFIX") or "/usr",
   url = "https://github.com/pakozm/april-ann",
   version_flags = {
     '-DAPRILANN_VERSION_MAJOR=0',
     '-DAPRILANN_VERSION_MINOR=4',
-    '-DAPRILANN_VERSION_RELEASE=0',
+    '-DAPRILANN_VERSION_RELEASE=1',
   },
   disclaimer_strings =  {
     '"APRIL-ANN v" TOSTRING(APRILANN_VERSION_MAJOR) "." TOSTRING(APRILANN_VERSION_MINOR) "." TOSTRING(APRILANN_VERSION_RELEASE) "  Copyright (C) 2012-2015 DSIC-UPV, CEU-UCH"',
